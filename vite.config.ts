67ab892a8b6b59e4d3eb30e54d9249eb4c5416af
harvey <info@ninescrolls.com>
2025-03-02 19:23:49 -0800

    add static pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  publicDir: 'public',
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/public/assets'
+    }
+  }
 })
