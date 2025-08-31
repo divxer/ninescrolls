2a2e0c7a29198bca2ac8273226071ab5f5bda73c
harvey <info@ninescrolls.com>
2025-08-30 21:36:20 -0700

    upgrade node.js version

--- amplify/functions/send-email/resource.ts
+++ amplify/functions/send-email/resource.ts
@@ -2,6 +2,7 @@ import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const sendEmail = defineFunction({
     entry: './handler.ts',
+    runtime: 20,
     environment: {
         SENDGRID_API_KEY: secret('SENDGRID_API_KEY'),
     },
