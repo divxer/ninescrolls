fcca12df5559a9e9bbb669bee2e12528bb6896b7
harvey <info@ninescrolls.com>
2026-01-13 16:24:48 -0800

    add new product: Compact RIE

--- src/components/products/CompactRIE.tsx
+++ src/components/products/CompactRIE.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useScrollToTop } from '../../hooks/useScrollToTop';
 import { DownloadGateModal } from '../common/DownloadGateModal';
 import { QuoteModal } from '../common/QuoteModal';
